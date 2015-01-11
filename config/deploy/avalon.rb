c02ffde94e11213b1b82c4479a076294a28e7105
lealeelu <lmb@iu.edu>
2015-01-11 06:31:55 -0500

    Update cap deploy script

--- config/deploy/avalon.rb
+++ config/deploy/avalon.rb
@@ -2,10 +2,10 @@ set :application, "avalon"
 set :repository,  "git://github.com/iu-avalon/avalon.git"
 set(:rails_env) { "production" }
 
-set(:deployment_host) { "avalon-pilot.dlib.indiana.edu" }  # Host(s) to deploy to
+set(:deployment_host) { "palm.dlib.indiana.edu" }  # Host(s) to deploy to
 set(:deploy_to) { "/var/www/avalon" }  # Directory to deploy into
 set(:user) { 'avalon' }                # User to deploy as
-set :branch, ENV['SCM_BRANCH'] || "release/3.0.0"       # Git branch to deploy
+set :branch, ENV['SCM_BRANCH'] || "iu-pilot-3.2"       # Git branch to deploy
 
 set :hls_dir, "/var/avalon/hls_streams"
 ssh_options[:keys] = ["/opt/staging/avalon/vov_deployment_key"]
