6b995ff3e09ca89add8c3f6a96418a404c9d8cd8
lealeelu <lmb@iu.edu>
2015-03-22 06:50:54 -0400

    Change avalon deploy version

--- config/deploy/avalon.rb
+++ config/deploy/avalon.rb
@@ -5,7 +5,7 @@ set :rails_env, ENV['RAILS_ENV'] || "production"
 set(:deployment_host) { "hickory.dlib.indiana.edu" }  # Host(s) to deploy to
 set(:deploy_to) { "/var/www/avalon" }  # Directory to deploy into
 set(:user) { 'avalon' }                # User to deploy as
-set :branch, ENV['SCM_BRANCH'] || "kelley-3.2"       # Git branch to deploy
+set :branch, ENV['SCM_BRANCH'] || "kelley-3.3"       # Git branch to deploy
 
 set :hls_dir, "/var/avalon/hls_streams"
 ssh_options[:keys] = ["/opt/staging/avalon/vov_deployment_key"]
